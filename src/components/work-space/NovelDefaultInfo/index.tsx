59799fa328dfa2936ac8e1d73b8fe93905e82534
Chooii <jiyechoo@gmail.com>
2024-10-17 19:37:31 +0900

    refactor: 소설공방 - 기본정보 탭 코드 정리

--- src/components/work-space/NovelDefaultInfo/index.tsx
+++ src/components/work-space/NovelDefaultInfo/index.tsx
@@ -8,7 +8,6 @@ import { useUrlDatas } from '@/hooks/useUrlDatas';
 
 import { ScrollTextBox } from '@/components';
 import Skel from '../../Skel/Skel';
-import st from './NovelDefaultInfo.module.scss';
 
 interface NovelDefaultInfoProps {
   isShow: boolean;
@@ -23,86 +22,90 @@ export const NovelDefaultInfo = ({ isShow = false }: NovelDefaultInfoProps) => {
     queryFn: () => novelRoomInfo(roomId),
   });
 
-  const toggleEditMode = () => {
-    setEditMode(prev => !prev);
-  };
-
-  const offEditMode = () => {
-    setEditMode(false);
-  };
+  const toggleEditMode = () => setEditMode(prev => !prev);
+  const offEditMode = () => setEditMode(false);
 
   return (
-    <div className={st.main} style={{ display: isShow ? 'flex' : 'none' }} aria-hidden={isShow}>
-      <div className={st.main_imgAndTextBox}>
-        {isSuccess && (
+    <div className={`flex-col w-full mt-2 ${isShow ? 'flex' : 'hidden'}`} aria-hidden={!isShow}>
+      <div className="flex flex-row">
+        {isSuccess ? (
           <Image
-            className={'object-cover'}
+            className="object-cover"
             width={270}
             height={202}
             src={novelInfo?.data?.bookCover ?? '/images/book-cover-2.png'}
             alt="북커버"
           />
+        ) : (
+          <Skel sx={{ width: 270, height: 202 }} />
         )}
-        {!isSuccess && <Skel sx={{ width: 270, height: 202 }} />}
 
-        <div className={st.main_textColumn}>
-          {isSuccess && (
+        <div className="flex flex-col ml-2">
+          {isSuccess ? (
             <ScrollTextBox
               disabled={!editMode}
               title="소개"
               content={novelInfo.data.subTitle}
-              style={{ width: '718px', height: '138px', marginLeft: '8px' }}
+              style={{ width: '718px', height: '138px' }}
             />
+          ) : (
+            <Skel sx={{ width: 718, height: 138 }} />
           )}
-          {!isSuccess && <Skel sx={{ width: 718, height: 138, marginLeft: 8 }} />}
 
-          {isSuccess && (
-            <div className={`${st.main_tagBox} ${editMode ? st.mdf : ''}`}>
-              <div className={st.main_tag_flexWrap}>
-                <div className={st.main_tagBox_tag}>#{novelInfo?.data.category.name}</div>
+          {isSuccess ? (
+            <div
+              className={`flex flex-row justify-between items-center w-[718px] h-14 mt-2 ml-2 px-8 py-3 rounded-lg bg-white/50 ${
+                editMode ? 'bg-white' : ''
+              }`}
+            >
+              <div className="flex flex-wrap">
+                <div className="flex items-center justify-center px-4 py-1 bg-white rounded-lg mr-2 text-gray-800 text-sm font-normal">
+                  #{novelInfo?.data.category.name}
+                </div>
               </div>
             </div>
+          ) : (
+            <Skel sx={{ width: 718, height: 56, marginLeft: 8, marginTop: 8 }} />
           )}
-          {!isSuccess && <Skel sx={{ width: 718, height: 56, marginLeft: 8, marginTop: 8 }} />}
         </div>
       </div>
 
-      {/* 등장인물 */}
-      {isSuccess && (
-        <ScrollTextBox
-          content={novelInfo.data.character}
-          disabled={!editMode}
-          title="등장인물"
-          style={{ width: '996px', height: '186px', marginTop: '8px' }}
-        />
+      {isSuccess ? (
+        <>
+          <ScrollTextBox
+            content={novelInfo.data.character}
+            disabled={!editMode}
+            title="등장인물"
+            style={{ width: '996px', height: '186px', marginTop: '8px' }}
+          />
+          <ScrollTextBox
+            content={novelInfo.data.summary}
+            disabled={!editMode}
+            title="줄거리"
+            style={{ width: '996px', height: '186px', marginTop: '8px' }}
+          />
+        </>
+      ) : (
+        <>
+          <Skel sx={{ width: '996px', height: '186px', marginTop: '8px' }} />
+          <Skel sx={{ width: '996px', height: '186px', marginTop: '8px' }} />
+        </>
       )}
-      {!isSuccess && <Skel sx={{ width: '996px', height: '186px', marginTop: '8px' }} />}
-
-      {/* 줄거리 */}
-      {isSuccess && (
-        <ScrollTextBox
-          content={novelInfo.data.summary}
-          disabled={!editMode}
-          title="줄거리"
-          style={{ width: '996px', height: '186px', marginTop: '8px' }}
-        />
-      )}
-      {!isSuccess && <Skel sx={{ width: '996px', height: '186px', marginTop: '8px' }} />}
 
       {editMode ? (
-        <div className={st.infoChangeBtn}>
-          <button onClick={offEditMode} className="blue-btn" type="button">
+        <div className="flex justify-center mt-8 gap-[2.87rem]">
+          <button onClick={offEditMode} className="blue-btn w-48 h-12" type="button">
             수정완료
           </button>
-          <button className="white-btn" type="button" onClick={offEditMode}>
+          <button onClick={offEditMode} className="white-btn w-48 h-12" type="button">
             취소
           </button>
         </div>
       ) : (
         <button
-          type="button"
-          className={`white-btn ${st.main_infoModifyBtn}`}
           onClick={toggleEditMode}
+          type="button"
+          className="white-btn w-48 h-12 self-center mt-8"
         >
           기본정보 수정
         </button>
