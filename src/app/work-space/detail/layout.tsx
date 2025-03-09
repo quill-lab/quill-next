9323940b035a118b7017723626797dd205e42609
striker1826 <alstjq123579@daum.net>
2025-03-09 21:04:39 +0900

    fix: work-info/layout

---
+++ src/app/work-space/detail/layout.tsx
@@ -0,0 +1,14 @@
+import { PageHeader } from '@/components/PageHeader/PageHeader';
+
+export default function DetailedWorkSpaceLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div
+      className={
+        "pt-[8px] pb-[72px] px-[124px] w-full bg-cover bg-[url('/images/back-image.svg')] min-h-screen flex flex-col gap-4 items-center overscroll-none"
+      }
+    >
+      <PageHeader />
+      <div className="flex gap-10 w-full">{children}</div>
+    </div>
+  );
+}
