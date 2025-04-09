592e822b6093faaea54eb3e09ecdc0f777925855
striker1826 <alstjq123579@daum.net>
2025-04-09 20:41:13 +0900

    fix: build error - add react icon size

--- src/pages/user/login/index.tsx
+++ src/pages/user/login/index.tsx
@@ -2,7 +2,8 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { FaEye, FaEyeSlash } from 'react-icons/fa6';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import type { IconType } from 'react-icons';
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 
@@ -108,7 +109,7 @@ export default function Login() {
                 onClick={() => setIsShowPassword(prev => !prev)}
                 className="bg-red-500 absolute right-[20px] top-1/2 transform -translate-y-1/2"
               >
-                {isShowPassword ? <FaEye /> : <FaEyeSlash />}
+                {isShowPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
               </button>
             </div>
             {errors.password && (
