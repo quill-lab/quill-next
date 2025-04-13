280b8357328fc378ec1297dd9ca263da68886240
striker1826 <alstjq123579@daum.net>
2025-04-13 15:02:19 +0900

    feat: 소설 공방 list mobile header에 padding 추가

--- src/app/work-space/(list)/layout.tsx
+++ src/app/work-space/(list)/layout.tsx
@@ -10,7 +10,7 @@ export default function DetailedWorkSpaceLayout({ children }: { children: React.
       </div>
 
       {/* MobileHeader for smaller screens */}
-      <div className="w-full block sm:hidden bg-[#9CE1E6] pt-[8px]">
+      <div className="w-full block sm:hidden bg-[#9CE1E6] pt-[8px] px-[16px]">
         <MobileHeader />
       </div>
       <div className="flex gap-10">{children}</div>
